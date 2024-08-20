e96ff7682b8b25c77ebc58ae0785d0b5361953bd
Lexal-dev <landoltalexis67@gmail.com>
2024-08-21 01:21:39 +0200

    adding servicesManager to update a services to employee

---
+++ app/login/auth/employee/servicesManager/page.tsx
@@ -0,0 +1,86 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+import { MdEdit } from 'react-icons/md';
+import FormUpdate from '@/components/services/employeeFormUpdate';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+interface Service {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export default function ServiceManager() {
+  const [services, setServices] = useState<Service[]>([]);
+  const [selectedService, setSelectedService] = useState<Service | null>(null);
+
+  const fetchServices = async (additionalParam: string | number) => {
+    try {
+      const response = await fetch(`/api/services/read?additionalParam=${encodeURIComponent(additionalParam.toString())}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch services');
+      }
+      const data = await response.json();
+      if (data.success) {
+        setServices(data.services);
+      } else {
+        throw new Error(data.message || 'Failed to fetch services');
+      }
+    } catch (error:any) {
+      console.error('Error fetching services:', error);
+      toast.error(`Erreur lors de la récupération des services: ${error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchServices('services'); // Replace 'default' with the actual parameter you need
+  }, []);
+
+  const handleEditService = (service: Service) => {
+    setSelectedService(service);
+  };
+
+  const handleUpdateSuccess = () => {
+    fetchServices('services'); // Replace 'default' with the actual parameter you need
+    setSelectedService(null);
+  };
+
+  return (
+    <main className="w-full flex-col flex py-12 px-2 items-center">
+
+      <div className="lg:w-2/3 overflow-x-auto  shadow-md md:rounded-lg">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-muted-foreground text-white">
+              <th className="w-1/5 px-4 py-2 text-left">Nom</th>
+              <th className="w-3/5 px-4 py-2 text-left">Description</th>
+              <th className="w-1/5 px-4 py-2 text-center">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {services.map(service => (
+              <tr key={service.id} className="border-t bg-foreground text-secondary hover:bg-muted hover:text-white">
+                <td className="w-1/5 px-4 py-2">{service.name}</td>
+                <td className="w-3/5 px-4 py-2">{service.description}</td>
+                <td className="w-full min-h-[100px] flex flex-col justify-center items-center">
+                  <button onClick={() => handleEditService(service)} className="text-yellow-500 hover:text-yellow-700">
+                    <MdEdit size={32} />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {selectedService && (
+        <FormUpdate
+          service={selectedService}
+          onUpdateSuccess={handleUpdateSuccess}
+          onClose={() => setSelectedService(null)}
+        />
+      )}
+    </main>
+  );
+}
---
+++ components/services/employeeFormUpdate.tsx
@@ -0,0 +1,119 @@
+"use client";
+import React, { useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { toast } from 'react-toastify';
+
+interface Service {
+  id: number;
+  name: string;
+  description: string;
+}
+
+interface ServiceUpdateFormProps {
+  service: Service;
+  onUpdateSuccess: () => void;
+  onClose: () => void;
+}
+
+const FormUpdate: React.FC<ServiceUpdateFormProps> = ({ service, onUpdateSuccess, onClose }) => {
+  const [formData, setFormData] = useState({
+    name: service.name,
+    description: service.description,
+  });
+
+  const [serverError, setServerError] = useState<string>('');
+
+  const MAX_NAME_LENGTH = 30;
+  const MAX_DESCRIPTION_LENGTH = 150;
+
+  const handleUpdateService = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Validation côté client pour la longueur des champs
+    if (formData.name.length < 3 || formData.name.length > MAX_NAME_LENGTH) {
+      return;
+    }
+
+    if (formData.description.length < 3 || formData.description.length > MAX_DESCRIPTION_LENGTH) {
+      return;
+    }
+
+    try {
+      const response = await fetch(`/api/services/update?id=${service.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: formData.name,
+          description: formData.description,
+        }),
+      });
+
+      const data = await response.json();
+      if (data.success) {
+        onUpdateSuccess(); // Appeler la fonction onUpdateSuccess
+        toast.success(`${formData.name} a bien été modifié`)
+      } else {
+        setServerError(data.message); // Capturer le message d'erreur du serveur
+      }
+    } catch (error) {
+      console.error('Error updating service:', error);
+    }
+  };
+
+  return (
+    <div className='fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50'>
+      <div className='flex flex-col justify-between bg-foreground p-6 rounded-lg text-secondary w-[600px] h-[400px]'>
+      <div className='text-xl flex items-center justify-between mb-6'>
+                    <p className='w-2/3 text-secondary font-bold '>Formulaire de création de service</p>
+                    <button onClick={onClose} className="w-1/3 flex justify-end text-red-500 hover:text-red-700"><MdClose size={36} /></button>                
+                </div>
+        <form onSubmit={handleUpdateService} className="flex flex-col h-full gap-6">
+      
+       
+            <input
+              type="text"
+              value={formData.name}
+              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              className=" text-white w-full p-2 rounded-md bg-muted hover:bg-background placeholder-slate-200"
+              placeholder='Entrez le nom du service ..'
+              maxLength={MAX_NAME_LENGTH}
+              required
+            />
+            <small className="text-red-500">
+              {formData.name.length < 3 && 'Le nom doit avoir au moins 3 caractères.'}
+              {formData.name.length > MAX_NAME_LENGTH && `Le nom ne doit pas dépasser ${MAX_NAME_LENGTH} caractères.`}
+            </small>
+          
+
+            <textarea
+              value={formData.description}
+              onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+              className="text-white w-full p-2 rounded-md bg-muted hover:bg-background placeholder-slate-200"
+              placeholder='Entrez le nom du service ..'
+              maxLength={MAX_DESCRIPTION_LENGTH}
+              required
+            />
+            <small className="text-red-500">
+              {formData.description.length < 3 && 'La description doit avoir au moins 3 caractères.'}
+              {formData.description.length > MAX_DESCRIPTION_LENGTH && `La description ne doit pas dépasser ${MAX_DESCRIPTION_LENGTH} caractères.`}
+            </small>
+     
+
+          <button
+            type="submit"
+            className="bg-muted hover:bg-muted-foreground text-white px-4 py-2 mt-4 rounded-md"
+          >
+            Mettre à Jour
+          </button>          
+          {serverError && (
+            <div className="text-red-500 mb-4" dangerouslySetInnerHTML={{ __html: serverError }} />
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default FormUpdate;
